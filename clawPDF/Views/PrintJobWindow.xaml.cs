76152400e8cde19c74812c7b24ae484b9d1b07cf
Daniel Obando <daniel.obando@netural.com>
2021-08-30 11:14:36 +0200

    chore: rolling back the last changes, null body parts are being handled from the BE

--- clawPDF/Views/PrintJobWindow.xaml.cs
+++ clawPDF/Views/PrintJobWindow.xaml.cs
@@ -44,8 +44,6 @@ namespace clawSoft.clawPDF.Views
 
         private void CommandButtons_OnClick(object sender, RoutedEventArgs e)
         {
-            SetBodyPartDefaultValue();
-
             DialogResult = true;
             Close();
         }
@@ -68,12 +66,6 @@ namespace clawSoft.clawPDF.Views
             {
                 BodyPartsBox.Items.Add(bp);
             }
-        }
-
-        private void SetBodyPartDefaultValue()
-        {
-            BodyPartsBox.SelectedIndex = 0;
-            BodyPartsBox.SelectedValue = BodyPartsBox.Items[0];
         }
     }
 }
